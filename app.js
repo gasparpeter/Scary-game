3e129641172d18ceb12def59d0da0b59210bb7b2
gasparpeter <petihappygaspar@gmail.com>
2019-10-08 16:56:55 +0300

    - lap2

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ let valami = document.getElementById('valami');
 let startBtn = document.getElementById('start-btn');
 
 let kocka1 = document.getElementById('kocka1');
-let finish = document.getElementById('finish1');
+let kocka2 = document.getElementById('kocka2');
+
+let finish1 = document.getElementById('finish1');
+let finish2 = document.getElementById('finish2');
 
 startBtn.addEventListener('click', function () {
    startPage.style.display = 'none';
@@ -18,11 +21,14 @@ kocka1.addEventListener('mouseout', function () {
    startMenu();
 });
 
-finish.addEventListener('mouseout', function () {
-
+finish1.addEventListener('mouseout', function () {
+   lap1.style.display = 'none';
+   lap2.style.display = 'block';
+   startPage.style.display = 'none';
 });
 
 function startMenu() {
    lap1.style.display = 'none';
+   lap2.style.display = 'none';
    startPage.style.display = 'block';
 }
